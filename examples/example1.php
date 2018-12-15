4feecc174d25f51e14185c3998992cae90a06ad9
jupitern <nunochaves@sapo.pt>
2018-12-15 21:04:46 +0000

    .

--- examples/example1.php
+++ examples/example1.php
@@ -4,23 +4,22 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 require __DIR__ .'/../vendor/autoload.php';
 
-echo "STEP: conn".PHP_EOL;
 
-$conn = new \Jupitern\CosmosDb\CosmosDb('host', 'pk');
+$conn = new \Jupitern\CosmosDb\CosmosDb(
+    'https://yeapp-cosmosdb.documents.azure.com:443/',
+    'CV3Hf1X2LASoenajh9QCEddyAPRgshSCGj8qfScmles4KHMhwsy4hAg36lHugVXmIxsh3ZZUmIS9hbC0xfN6Dw=='
+);
 
-echo "STEP: select db".PHP_EOL;
 $db = $conn->selectDB('yeapp-staging');
-
-echo "STEP: select collection".PHP_EOL;
 $collection = $db->selectCollection('Users');
 
-//        $rid = \Jupitern\CosmosDb\QueryBuilder::instance()
-//            ->setDatabase($db)
-//            ->setCollection($collection)
-//            ->setPartitionKey('collectiontype')
-//            ->save(['id' => 'user1', 'name' => 'john doe', 'age' => 30, 'collectiontype' => 'user']);
-//
-//        echo ($rid != null ? "user saved" : "user not saved").PHP_EOL;
+$rid = \Jupitern\CosmosDb\QueryBuilder::instance()
+    ->setDatabase($db)
+    ->setCollection($collection)
+    ->setPartitionKey('collectiontype')
+    ->save(['id' => 'user1', 'name' => 'john doe', 'age' => 30, 'collectiontype' => 'user']);
+
+echo ($rid != null ? "user saved" : "user not saved").PHP_EOL;
 
 echo "STEP: single partition query".PHP_EOL;
 
@@ -41,7 +40,6 @@ foreach ($users as $user) {
 
 echo "STEP: multi partition query".PHP_EOL;
 
-
 // select users with age 30 with multi partition query
 $users = \Jupitern\CosmosDb\QueryBuilder::instance()
     ->setDatabase($db)
@@ -57,20 +55,20 @@ foreach ($users as $user) {
     var_dump($user);
 }
 
-//        // delete a user
-//        \Jupitern\CosmosDb\QueryBuilder::instance()
-//            ->setCollection($collection)
-//            ->setDatabase($db)
-//            ->setPartitionKey('collectiontype')
-//            ->where("c.id = 'user123' and c.collectiontype = 'user'")
-//            ->delete();
-//
-//        // delete all users aged 30
-//        \Jupitern\CosmosDb\QueryBuilder::instance()
-//            ->setCollection($collection)
-//            ->setDatabase($db)
-//            ->setPartitionKey('collectiontype')
-//            ->where("c.age = 30 and c.collectiontype = 'user'")
-//            ->deleteAll();
-//
-//        echo "finished";
+// delete a user
+\Jupitern\CosmosDb\QueryBuilder::instance()
+    ->setCollection($collection)
+    ->setDatabase($db)
+    ->setPartitionKey('collectiontype')
+    ->where("c.id = 'user123' and c.collectiontype = 'user'")
+    ->delete();
+
+// delete all users aged 30
+\Jupitern\CosmosDb\QueryBuilder::instance()
+    ->setCollection($collection)
+    ->setDatabase($db)
+    ->setPartitionKey('collectiontype')
+    ->where("c.age = 30 and c.collectiontype = 'user'")
+    ->deleteAll();
+
+echo "finished";
